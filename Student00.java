62e7113baf17a36e9c72d6d7969adb9cc1b04bc0
Patrick Drumm <pdrumm@nd.edu>
2017-02-02 15:08:27 -0500

    Update Student00.java

--- Student00.java
+++ Student00.java
@@ -5,46 +5,45 @@ import Social.*;
 import School.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.Random;
 
 class Student00 extends CSEStudent {
 
 	Student00() {
-  	activities = {"Band", "School work", "Studying"};
-  	spirituality = "agnostic";
-  	freeTime = {"golfing"}; 
+  		activities = {"Band", "School work", "Studying"};
+  		spirituality = "agnostic";
+  		freeTime = {"golfing"}; 
 	}
 	
 	@Override
 	void graduateFromND() {
-	  PostGrad.education.goToGradSchool();
+		PostGrad.education.goToGradSchool();
 	};
 	
 	@Override
 	void encounterSocialSituation() {
-	  Social.encounter.hide();
+		Social.encounter.hide();
 	};
 	
 	@Override
 	void study() {
-	  School.homework.complete();
-	  for (int i=0; i<3; i++) {
-	    School.homework.checkAnswers();
-	  }
-	  School.homework.turnin();
+		School.homework.complete();
+		for (int i=0; i<3; i++) {
+			School.homework.checkAnswers();
+		}
+		School.homework.turnin();
 	};
 	
 	@Override
 	public void spendFreeTime() {
-	  String pattern = "(study|school|work)";
-	  Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+		String pattern = "(study|school|work)";
+		Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
 	  
-	  for (String activity : self.activities) {
-	    Matcher m = r.matcher(activity);
-	    if (m.find( )) {
-	      School.freeTime.spendOn(activity);
-	    }
-	  }
+		for (String activity : self.activities) {
+			Matcher m = r.matcher(activity);
+			if (m.find( )) {
+				School.freeTime.spendOn(activity);
+			}
+		}
 	};
 
 }
