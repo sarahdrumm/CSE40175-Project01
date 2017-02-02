6b7848185096c3a4088a75423dc8ee9278fb749e
kthienem <kthienem@nd.edu>
2017-02-02 10:54:02 -0500

    Add Student00

---
+++ Student00.java
@@ -0,0 +1,41 @@
+class Student00 extends CSEStudent {
+
+	Student00() {
+  	activities = {"Band", "School work", "Studying"};
+  	spirituality = "agnostic";
+  	freeTime = {"golfing"}; 
+	}
+	
+	@Override
+	void graduateFromND() {
+	  PostGrad.education.goToGradSchool();
+	};
+	
+	@Override
+	void encounterSocialSituation() {
+	  Social.encounter.hide();
+	};
+	
+	@Override
+	void study() {
+	  School.homework.complete();
+	  for (int i=0; i<3; i++) {
+	    School.homework.checkAnswers();
+	  }
+	  School.homework.turnin();
+	};
+	
+	@Override
+	public void spendFreeTime() {
+	  String pattern = "(study|school|work)";
+	  Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+	  
+	  for (String activity : self.activities) {
+	    Matcher m = r.matcher(activity);
+	    if (m.find( )) {
+	      School.freeTime.spendOn(activity);
+	    }
+	  }
+	};
+
+}
