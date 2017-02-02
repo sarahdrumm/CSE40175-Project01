a8a24b217da8c66c7a5ed200e2da859c8b69a86f
kthienem <kthienem@nd.edu>
2017-02-02 11:06:03 -0500

    Add Methods

--- CSEStudent.java
+++ CSEStudent.java
@@ -10,10 +10,20 @@ class CSEStudent {
     String ethnicity = "white";
     String spirituality = "Roman Catholic";
     String minor = null;
+    String[] activities;
+    String[] freetime;
     
     /* 
     *   Methods
     */
     
     CSEStudent () {};
+        
+    public void graduateFromND() {
+        //What now?
+    };
+        
+    public void study() {};
+        
+    public void spendFreeTime() {};
 };
