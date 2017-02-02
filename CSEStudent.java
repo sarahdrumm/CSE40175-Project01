d1f2ae8e495100dbe011493487498bedf3d6d820
Sarah Elizabeth Drumm <sdrumm@nd.edu>
2017-02-02 10:15:51 -0500

    Create CSEStudent

---
+++ CSEStudent.java
@@ -0,0 +1,19 @@
+Package com.notreDame;
+
+class CSEStudent {
+        
+    /*
+    *  Attributes
+    */
+    String gender = "male";
+    String origin = "midwest";
+    String ethnicity = "white";
+    String spirituality = "Roman Catholic";
+    String minor = null;
+    
+    /* 
+    *   Methods
+    */
+    
+    CSEStudent () {};
+};
