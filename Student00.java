5215fbedeffadca9be87a89989e2ae4b895984bd
Patrick Drumm <pdrumm@nd.edu>
2017-02-02 15:14:24 -0500

    Update Student00.java

--- Student00.java
+++ Student00.java
@@ -8,42 +8,42 @@ import java.util.regex.Pattern;
 
 class Student00 extends CSEStudent {
 
-	Student00() {
-  		activities = {"Band", "School work", "Studying"};
-  		spirituality = "agnostic";
-  		freeTime = {"golfing"}; 
-	}
-	
-	@Override
-	void graduateFromND() {
-		PostGrad.education.goToGradSchool();
-	};
-	
-	@Override
-	void encounterSocialSituation() {
-		Social.encounter.hide();
-	};
-	
-	@Override
-	void study() {
-		School.homework.complete();
-		for (int i=0; i<3; i++) {
-			School.homework.checkAnswers();
-		}
-		School.homework.turnin();
-	};
-	
-	@Override
-	public void spendFreeTime() {
-		String pattern = "(study|school|work)";
-		Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
-	  
-		for (String activity : self.activities) {
-			Matcher m = r.matcher(activity);
-			if (m.find( )) {
-				School.freeTime.spendOn(activity);
-			}
-		}
-	};
+    Student00() {
+        activities = {"Band", "School work", "Studying"};
+        spirituality = "agnostic";
+        freeTime = {"golfing"}; 
+    }
+
+    @Override
+    void graduateFromND() {
+        PostGrad.education.goToGradSchool();
+    };
+
+    @Override
+    void encounterSocialSituation() {
+        Social.encounter.hide();
+    };
+
+    @Override
+    void study() {
+        School.homework.complete();
+        for (int i=0; i<3; i++) {
+            School.homework.checkAnswers();
+        }
+        School.homework.turnin();
+    };
+
+    @Override
+    public void spendFreeTime() {
+        String pattern = "(study|school|work)";
+        Pattern r = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE);
+
+        for (String activity : self.activities) {
+            Matcher m = r.matcher(activity);
+            if (m.find( )) {
+                School.freeTime.spendOn(activity);
+            }
+        }
+    };
 
 }
