10a8ab5bca4bfdfaa31656191e0b127d1f9d850a
Patrick Drumm <pdrumm@nd.edu>
2017-02-02 15:04:29 -0500

    Update CSEStudent.java

--- CSEStudent.java
+++ CSEStudent.java
@@ -1,22 +1,34 @@
 Package com.notreDame;
 
 class CSEStudent {
+    
+    protected class Clothes {
+        String top;
+        String bottom;
+        Clothes(String top, String bottom) {
+            self.top = top;
+            self.bottom = bottom;
+        }
+        void addLayer(String top) {
+            self.top += " and " + top;
+        }
+    }
         
     /*
-    *  Attributes
-    */
+     *  Attributes
+     */
     String gender = "male";
     String origin = "midwest";
     String ethnicity = "white";
     String spirituality = "Roman Catholic";
+    Clothes myClothes = new Clothes("t-shirt", "jeans");
     String minor = null;
     String[] activities;
     String[] freetime;
     
     /* 
-    *   Methods
-    */
-    
+     *   Methods
+     */
     CSEStudent () {};
         
     public void graduateFromND() {
