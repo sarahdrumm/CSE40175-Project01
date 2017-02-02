5153fdeb9f2b7f99b93d3ad604b2f28945e4c6b4
Patrick Drumm <pdrumm@nd.edu>
2017-02-02 15:10:38 -0500

    Update Student01.java

--- Student01.java
+++ Student01.java
@@ -1,36 +1,43 @@
+package com.notreDame;
+
+import PostGrad.*;
+import Social.*;
+import School.*;
+import java.util.Random;
+
 class Student01 extends CSEStudent {
 
-  Student01 () {
-    origin = "West";
-  	ethnicity = "Asian";
-  	minor = "Engineering in Corporate Practice";
-  	activities = {"Slack", "Video games", "Ultimate frisbee", "Science Fiction", "Hackathons"};
-  	spirituality = "Sometimes I think ghosts exist";
-  	freeTime = {"Longboard", "Chill"};
-	
-	  self.myClothes.addLayer("Google hoodie");
-	}
-	
-	@Override
-	void graduateFromND() {
-	  PostGrad.realPersonWorld.getJob("Seattle");
-	};
-	
-	@Override
-	void encounterSocialSituation() {
-	  Social.encounter.clashRoyale();
-	};
-	
-	@Override
-	void study() {
-	  // null
-	};
-	
-	@Override
-	public void spendFreeTime() {
-	  Random rand = new Random();
-	  int n = rand.nextInt(3);
-	  School.freeTime.spendOn(self.activites[n]);
-	};
+    Student01 () {
+        origin = "West";
+        ethnicity = "Asian";
+        minor = "Engineering in Corporate Practice";
+        activities = {"Slack", "Video games", "Ultimate frisbee", "Science Fiction", "Hackathons"};
+        spirituality = "Sometimes I think ghosts exist";
+        freeTime = {"Longboard", "Chill"};
+
+        self.myClothes.addLayer("Google hoodie");
+    }
+
+    @Override
+    void graduateFromND() {
+        PostGrad.realPersonWorld.getJob("Seattle");
+    };
+
+    @Override
+    void encounterSocialSituation() {
+        Social.encounter.clashRoyale();
+    };
+
+    @Override
+    void study() {
+        // null
+    };
+
+    @Override
+    public void spendFreeTime() {
+        Random rand = new Random();
+        int n = rand.nextInt(3);
+        School.freeTime.spendOn(self.activites[n]);
+    };
 
 }
